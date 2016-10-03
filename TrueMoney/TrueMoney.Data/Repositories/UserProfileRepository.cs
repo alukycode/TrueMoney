a73b507593c68711747831300da2ddf5a5866250
DESKTOP-5HPHMB2\facep <f.acepalm@mail.ru>
2016-10-03 23:48:43 +0300

    Added infrasturcture projects.

---
+++ TrueMoney/TrueMoney.Data/Repositories/UserProfileRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrueMoney.Infrastructure.Entities;
+using TrueMoney.Infrastructure.Repositories;
+
+namespace TrueMoney.Data.Repositories
+{
+    public class UserProfileRepository : IUserProfileRepository
+    {
+        public IEnumerable<UserProfile> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
