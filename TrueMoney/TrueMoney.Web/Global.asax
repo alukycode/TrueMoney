87a2a0ed3e520e3aa59c5cd6ac82ae690a53d03c
Dmitry Artux <d.artuh@colours.nl>
2016-10-03 22:55:35 +0300

    create vs project

---
+++ TrueMoney/TrueMoney.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrueMoney.Web.MvcApplication" Language="C#" %>
---
+++ TrueMoney/TrueMoney.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrueMoney.Web.Startup))]
+namespace TrueMoney.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
