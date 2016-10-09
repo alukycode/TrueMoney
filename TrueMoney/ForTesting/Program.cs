6d90760fb6ad58accfe269c34657bf1dc6c6da8e
DESKTOP-5HPHMB2\facep <f.acepalm@mail.ru>
2016-10-09 16:14:48 +0300

    еуфыва

---
+++ TrueMoney/ForTesting/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using TrueMoney.Data;
+using TrueMoney.Data.Entities;
+using TrueMoney.Data.Repositories;
+using TrueMoney.Mapping;
+
+namespace ForTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new TrueMoneyContext())
+            {
+                context.Users.Add(new User() { Name = "temp" });
+                context.SaveChanges();
+            }
+        }
+    }
+}
