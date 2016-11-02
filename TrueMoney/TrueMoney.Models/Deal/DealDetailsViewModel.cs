9d25e22f9803db67aaf1eef8961ac583bf62b126
Dmitry Artux <d.artuh@colours.nl>
2016-11-02 12:29:57 +0300

    revert changes

--- TrueMoney/TrueMoney.Models/Deal/DealDetailsViewModel.cs
+++ TrueMoney/TrueMoney.Models/Deal/DealDetailsViewModel.cs
@@ -11,9 +11,13 @@
     {
         public DealModel Deal { get; set; }
 
+        public bool IsCurrentUserBorrower { get; set; }
+
         public int CurrentUserId { get; set; }
 
         public bool IsCurrentUserLender { get; set; }
+
+        public bool ShowOffers => IsCurrentUserBorrower;
 
         public OfferModel CurrentUserOffer
         {
